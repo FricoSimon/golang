dd77532824b91c07fe197ee87e6986b71b5a03ad
FricoSimon <simonfriko@gmail.com>
2023-07-26 21:15:55 +0700

    add explanation comments

--- hello.go
+++ hello.go
@@ -3,17 +3,25 @@ package main // only one main per package
 import "fmt" // for console
 
 func main () {
+	// print to console
 	fmt.Print("Hello ")
 	fmt.Println("World!")
 
+	// string interpolation
 	name := "Frico" // declare variable
 	fmt.Println("Hello", name)
 	fmt.Printf("Hello %s \n", name) // use %s for string
 
+	// concatenate strings
 	fname := "Friko"
 	lname := "Simon"
-	fullName := fname + " " + lname // concatenate strings
+	fullName := fname + " " + lname 
 
 	fmt.Println(fullName)
+
+	// sum of two numbers
+	a := 10
+	b := 2
+	fmt.Println(a + b) 
 
 }
